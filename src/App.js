637b8ca580e29bf684a19e72511deb2fb12bc2d1
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-07-05 16:12:14 -0600

    adding new api, setting useEffect

--- src/App.js
+++ src/App.js
@@ -2,104 +2,48 @@ import React, { useState, useEffect } from 'react';
 import './index.css';
 
 function App() {
-  const[country, setCountry] = useState('');
-  const[service, setService] = useState('');
-  const[type, setType] = useState('');
-  const[genre, setGenre] = useState('');
-  const[outputLanguage, setOutputLanguage] = useState('');
-  const[language, setLanguage] = useState('');
-  const[page, setPage] = useState(1);
+  const[t, setTitle] = useState('');
+  const[container, setContainer] = useState([]);
+  const[finalPoint, setFinalPoint] = useState('');
 
-  const genres = {
-    "1":"Biography",
-    "3":"Game Show",
-    "2":"Film Noir",
-    "4":"Musical",
-    "5":"Sport",
-    "6":"Short",
-    "7":"Adult",
-    "12":"Adventure",
-    "14":"Fantasy",
-    "16":"Animation",
-    "18":"Drama",
-    "27":"Horror",
-    "28":"Action",
-    "35":"Comedy",
-    "36":"History",
-    "37":"Western",
-    "53":"Thriller",
-    "80":"Crime",
-    "99":"Documentary",
-    "878":"Science Fiction",
-    "9648":"Mystery",
-    "10402":"Music",
-    "10749":"Romance",
-    "10751":"Family",
-    "10752":"War",
-    "10763":"News",
-    "10764":"Reality",
-    "10767":"Talk Show",
-  };
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '0f29cad798msh47149466314dbd6p13b13cjsne4a16cf5edc8',
-      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-    }
-
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
   }
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
+  
+  const fetchMe = () => {
+    fetch(`http://www.omdbapi.com/?apikey=e76c98b5&s=${t}`)
+    .then(response => { return response.json();})
+    .then(data => {setContainer(data)})
+    .then(console.log(container))
+    
+    .catch(err => console.error(err));
   }
-
-  const onChangeService = (e) => {
-    setService(e.target.value);
-  }
-  const onChangeType = (e) => {
-    setType(e.target.value);
-  }
-
-  const onChangeGenre = (e) => {
-    setGenre(e.target.value);
-  }
-  const onChangeOutputLanguage = (e) => {
-    setOutputLanguage(e.target.value);
-  }
-
-  const onChangeLanguage = (e) => {
-    setLanguage(e.target.value);
-  }
+  
+  useEffect(() => {
+    fetchMe()
+  }, [finalPoint])
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
-  
-    fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=${country}&service=${service}&type=${type}&genre=${genre}&outpult_language=${outputLanguage}&language=${language}`, options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
+    setFinalPoint(t);
   }
   return (
     <div className="App">
       <form action="">
-        <input type="text" value={country} onChange={onChangeCountry} />
-        <input type="text" value={service} onChange={onChangeService} />
-        <input type="text" value={type} onChange={onChangeType} />
-        <select value={genre} onChange={onChangeGenre}>
-          {Object.values(genres).map((genre, index)=> {
-            return <option key={index} value={genre}>{genre}</option>
-          })}
-        </select>
-        <select value={outputLanguage} onChange={onChangeOutputLanguage}>
-          <option value="">Select</option>
-          <option value="en">en</option>
-          <option value="es">es</option>
-        </select>
-        <input type="text" value={language} onChange={onChangeLanguage} />
+        <input type="text" value={t} onChange={onChangeTitle} />
         <button type="submit" onClick={onSubmitHandler}>Submit</button>
       </form>
+      {container.Search && container.Search.map((item, index) => {
+        return(
+          <div key={index}>
+            {item.Title}
+            <img src={item.Poster} alt="" />
+          </div>
+        )
+      })}
+      <div>showing {container.Search && container.Search.length} of {container.totalResults} results</div>
     </div>
-  );
+  )
 }
 
 export default App;
